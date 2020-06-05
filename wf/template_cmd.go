5d18df0d6caa93799f527ad7713208d702b7a165
tony <ayakovlenko@users.noreply.github.com>
2020-06-05 13:34:22 +0300

    Cleanup

--- wf/template_cmd.go
+++ wf/template_cmd.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 	"wf"
 
@@ -98,4 +101,52 @@ func parseTemplateParam(arg string) (key string, value interface{}) {
 	key = arg[:idx]
 	value = arg[idx+1:]
 	return
+}
+
+func locateTemplateDir() (string, error) {
+	wfDir, ok := os.LookupEnv("WF_DIR")
+	if !ok {
+		return "", errors.New("WF_DIR is not defined")
+	}
+
+	return filepath.Join(wfDir, "templates"), nil
+}
+
+func loadTemplate(name string, env map[string]interface{}) (*wf.Item, error) {
+	tplDir, err := locateTemplateDir()
+	if err != nil {
+		return nil, err
+	}
+
+	filename := filepath.Join(tplDir, name+".js")
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error opening file: %s", filename)
+	}
+
+	return wf.EvalTemplate(name+".js", f, env)
+}
+
+func getAvailableTemplates() ([]string, error) {
+	tplDir, err := locateTemplateDir()
+	if err != nil {
+		return nil, err
+	}
+
+	files, err := ioutil.ReadDir(tplDir)
+	if err != nil {
+		return nil, err
+	}
+
+	tpls := []string{}
+	for _, f := range files {
+		name := f.Name()
+
+		if strings.HasSuffix(name, ".js") {
+			tpls = append(tpls, strings.TrimSuffix(name, ".js"))
+		}
+	}
+
+	return tpls, nil
 }
